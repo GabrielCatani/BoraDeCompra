749b8002bd45afb075ff1928f1d2d01b2483a217
Gabriel Catani <gabrielm.catani@gmail.com>
2023-08-23 09:38:09 -0300

    initial commit.

---
+++ src/main/java/com/BoraDeCompra/BoraDeCompraApplication.java
@@ -0,0 +1,13 @@
+package com.BoraDeCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraDeCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraDeCompraApplication.class, args);
+	}
+
+}
