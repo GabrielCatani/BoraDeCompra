70aaeda28b96edc8e4d55119663b3ee594634653
Gabriel Catani <gabrielm.catani@gmail.com>
2023-08-24 08:43:49 -0300

    Add Swagger dependency for ease of initial testing of routes, and API documentation. Updates `UserService` implementing methods (WIP)

--- src/main/java/com/BoraDeCompra/controller/UserController.java
+++ src/main/java/com/BoraDeCompra/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.BoraDeCompra.controller;
 
+import com.BoraDeCompra.DTO.UserDTO;
 import com.BoraDeCompra.entity.UserEntity;
+import com.BoraDeCompra.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,26 +12,35 @@ import java.util.List;
 
 @RestController()
 public class UserController {
-    @GetMapping("/users")
-    List<UserEntity> getAllUsers() {
-       return new ArrayList<UserEntity>();
+
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users/{id}")
-    UserEntity getOneUser(@PathVariable Long id) {
-        System.out.println(id);
-        return new UserEntity();
+    ResponseEntity<UserDTO> getOneUser(@PathVariable Long id) {
+        UserDTO userDTO = this.userService.findById(id);
+        if (userDTO == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(userDTO, HttpStatus.FOUND);
+    }
+    @GetMapping("/users")
+    List<UserDTO> getAllUsers() {
+        return new ArrayList<UserDTO>();
     }
 
     @PostMapping("/users")
-    UserEntity createUser(@RequestBody UserEntity user) {
-        return new UserEntity();
+    UserDTO createUser(@RequestBody UserDTO userDTO) {
+        return new UserDTO();
     }
 
     @PutMapping("/users/{id}")
-    UserEntity updateUser(@RequestBody UserEntity user, @PathVariable Long id) {
+    UserDTO updateUser(@RequestBody UserDTO userDTO, @PathVariable Long id) {
         System.out.println(id);
-        return new UserEntity();
+        return new UserDTO();
     }
 
     @DeleteMapping("/users/{id}")
--- src/main/java/com/BoraDeCompra/service/UserService.java
+++ src/main/java/com/BoraDeCompra/service/UserService.java
@@ -1,9 +1,12 @@
 package com.BoraDeCompra.service;
 
 import com.BoraDeCompra.DTO.UserDTO;
+import com.BoraDeCompra.entity.UserEntity;
 import com.BoraDeCompra.mapper.UserMapper;
 import com.BoraDeCompra.repository.UserRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,5 +16,15 @@ public class UserService {
     public UserService(UserRepo userRepo, UserMapper userMapper) {
         this.userRepo = userRepo;
         this.userMapper = userMapper;
+    }
+
+    public UserDTO findById(Long id) {
+        Optional opt = this.userRepo.findById(id);
+        UserEntity user = null;
+
+        if (opt.isPresent()) {
+            user = (UserEntity) opt.get();
+        }
+        return this.userMapper.toUserDTO(user);
     }
 }
